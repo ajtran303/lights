aec5d46b1b76983ff4fe3032ea4f487270892d14
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-07-06 03:44:49 -0400

    Add light test

---
+++ light_test.py
@@ -0,0 +1,46 @@
+import unittest
+
+class LightTest(unittest.TestCase):
+    """
+    - There is a light
+    - It can turn on and off
+    - It can shine light or not
+    - When a light shines, it emits lumens
+    """
+
+    # a light exists with lumens
+    # a light is unshone and does not emit lumens
+    # a light turns on
+    # a light is shone and emits lumens
+    # a light turns off
+    # a light is unshone and does not emit lumens anymore
+
+    def setUp(self):
+        """A light is initialized with lumens"""
+        self.light = Light(lumens=100)
+
+    # def tearDown(self):
+        # self.light.power_off() # ?
+
+    def test_it_exists(self):
+        """Smoke test"""
+        self.assertIsInstance(self.light, Light)
+
+    def test_it_does_not_shine_unless_powered(self):
+        """Integration test"""
+        self.assertEqual(self.light.is_on, (False))
+        self.assertEqual(self.light.shine(), (0))
+
+        self.light.power_on()
+        self.assertEqual(self.light.is_on, (True))
+        self.assertEqual(self.light.shine(), (100))
+
+        self.light.power_off()
+        self.assertEqual(self.light.is_on, (False))
+        self.assertEqual(self.light.shine(), (0))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
